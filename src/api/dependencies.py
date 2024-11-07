6518639d89d88ba2dece5d9e4e3d359119abeb8d
Valery Shkarubin <valis660@mail.ru>
2024-11-07 12:31:11 +0300

    create UserIdDep

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,7 +1,10 @@
 from typing import Annotated
 
-from fastapi import Depends, Query
+from fastapi import Depends, Query, Request, HTTPException
 from pydantic import BaseModel
+
+from src.services.auth import AuthService
+
 
 class PaginationParams(BaseModel):
     page: Annotated[int | None, Query(1, ge=1)]
@@ -9,3 +12,18 @@ class PaginationParams(BaseModel):
 
 
 PaginationDep = Annotated[PaginationParams, Depends()]
+
+
+def get_token(request: Request) -> str:
+    token = request.cookies.get("access_token")
+    if not token:
+        raise HTTPException(status_code=401, detail="Вы не предоставили токен")
+    return token
+
+
+def get_current_user_id(token: str = Depends(get_token)) -> int:
+    data = AuthService().decode_token(token)
+    return data["user_id"]
+
+
+UserIdDep = Annotated[int, Depends(get_current_user_id)]
