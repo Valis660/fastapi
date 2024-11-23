10dd5ec818287787679c404ee99ff33fb7d33811
Valery Shkarubin <valis660@mail.ru>
2024-11-23 16:48:44 +0300

    create DataMapper and HotelMapper

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -4,13 +4,14 @@ from datetime import date
 from src.models.rooms import RoomsOrm
 from src.repositories.base import BaseRepository
 from src.models.hotels import HotelsOrm
+from src.repositories.mappers.mappers import HotelDataMapper
 from src.repositories.utils import rooms_ids_for_booking
 from src.schemas.hotels import Hotel
 
 
 class HotelsRepository(BaseRepository):
     model = HotelsOrm
-    schema = Hotel
+    mapper = HotelDataMapper
 
 
     async def get_filtered_bytime(
@@ -41,5 +42,5 @@ class HotelsRepository(BaseRepository):
         )
         result = await self.session.execute(query)
 
-        return [self.schema.model_validate(model, from_attributes=True) for model in result.scalars().all()]
+        return [self.mapper.map_to_domain_entity(model) for model in result.scalars().all()]
 
