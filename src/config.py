effecc137777ec93cf3ca03adde58f20a070a206
Valery Shkarubin <valis660@mail.ru>
2025-01-04 18:05:50 +0300

    add pytest-dotenv, .env-test

--- src/config.py
+++ src/config.py
@@ -2,6 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    MODE: Literal["TEST", "LOCAL", "DEV", "PROD"]
+
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
